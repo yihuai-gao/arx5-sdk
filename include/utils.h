8e5c541fa9439a4471b18b8f9de0391e10c7eb31
Yihuai Gao <yihuai@stanford.edu>
2024-06-21 20:16:26 -0700

    Add more safety features

--- include/utils.h
+++ include/utils.h
@@ -1,7 +1,7 @@
 #ifndef UTILS_H
 #define UTILS_H
+#include <Eigen/Core>
 #include "app/common.h"
-
 class MovingAverage6d {
  public:
   MovingAverage6d(int window_size);
@@ -17,5 +17,8 @@ class MovingAverage6d {
   Vec6d _window_sum;
   Vec6d* _window;
 };
+
+std::string vec2str(const Eigen::VectorXd& vec, int precision = 3);
+std::string vec2str(const Vec6d& vec, int precision = 3);
 
 #endif
