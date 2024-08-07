36573ad105078fa27f6e7df9f6b4f86999c510e1
Yihuai Gao <yihuai@stanford.edu>
2024-08-07 19:34:47 +0800

    Remove redundant functions in motor.h

--- include/hardware/motor.h
+++ include/hardware/motor.h
@@ -1,29 +1,10 @@
 #ifndef MOTOR_H
 #define MOTOR_H
 
-#define param_get_pos 0x01
-#define param_get_spd 0x02
-#define param_get_cur 0x03
-#define param_get_pwr 0x04
-#define param_get_acc 0x05
-#define param_get_lkgKP 0x06
-#define param_get_spdKI 0x07
-#define param_get_fdbKP 0x08
-#define param_get_fdbKD 0x09
-
-#define comm_ack 0x00
-#define comm_auto 0x01
-
 #include "math_ops.h"
 #include <array>
 #include <iostream>
 #include <stdint.h>
-typedef struct
-{
-    uint16_t motor_id;
-    uint8_t INS_code;  // instruction code.
-    uint8_t motor_fbd; // motor CAN communication feedback.
-} MotorCommFbd;
 
 typedef struct
 {
@@ -45,20 +26,9 @@ typedef struct
 
 } OD_Motor_Msg;
 
-void MotorSetting(uint16_t motor_id, uint8_t cmd);
-void MotorIDReading(uint8_t *pData, uint32_t *pCanID, uint8_t *pDataBufferLen);
-void GetMotorParameter(uint16_t motor_id, uint8_t param_cmd, uint8_t *pData, uint32_t *pCanID, uint8_t *pDataBufferLen);
-void send_motor_ctrl_cmd(uint16_t motor_id, float kp, float kd, float pos, float spd, float tor, uint8_t *Data,
-                         uint32_t *canID);
-void send_motor_dm_cmd(uint16_t motor_id, float kp, float kd, float pos, float spd, float tor, uint8_t *Data,
-                       uint32_t *canID);
-
-void Gripper_can_data_repack(uint32_t msgID, uint8_t *Data, std::array<OD_Motor_Msg, 10> &motor_msg);
-
+void gripper_can_data_repack(uint32_t msgID, uint8_t *Data, std::array<OD_Motor_Msg, 10> &motor_msg);
 void RV_can_data_repack(uint32_t msgID, uint8_t *Data, int32_t databufferlen, uint8_t comm_mode,
                         std::array<OD_Motor_Msg, 10> &motor_msg);
 void DM_can_data_repack(uint8_t *Data, std::array<OD_Motor_Msg, 10> &motor_msg);
-void send_motor_position(uint16_t motor_id, float pos, uint16_t spd, uint16_t cur, uint8_t ack_status, uint8_t *Data,
-                         uint32_t *canID);
 
 #endif
